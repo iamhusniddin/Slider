9633b2a18b3797bc1bf9d7c02aacb2a4008a5f8b
Husniddin <122876021+iamhusniddin@users.noreply.github.com>
2023-08-24 10:53:40 +0500

    first commit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,9 +5,9 @@ function Modal({setModal}) {
     <div className='modal-back'>
         <div className='modal'>
             <img className='modalImg' onClick={()=>setModal(false)} width='35px' src="https://img.favpng.com/6/17/8/greek-alphabet-letter-case-x-png-favpng-cHuZ2q2FSYJixmFrb7gtTP1XA.jpg" alt="" />
-            <p className='modalText'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Dolor distinctio aliquam deserunt, excepturi est sed vel suscipit animi facere autem?
-            </p>
+            <form action="">
+              <input type="text" />
+            </form>
             <button className='modalBtn' onClick={()=>setModal(false)}>Send</button>
         </div>
     </div>
